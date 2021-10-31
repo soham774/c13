2c7927a87c923d0181d145688c2afa76db6f7e90
soham774 <90966868+soham774@users.noreply.github.com>
2021-10-31 14:41:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -39,55 +39,25 @@ function draw() {
   
 // var select_sprites = Math(random(1,3));
 
-// var select_sprites = Math.random(random(1,3));
+ //var select_sprites = Math.random(random(1,3));
 
-// var select_sprites = Math.round(1,3);
+ //var select_sprites = Math.round(1,3);
 
-// var select_sprites = Math.round(random(1,3));
+ var select_sprites = Math.round(random(1,3));
 
   
-  // if (frameCount % 50 == 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
-  // }
-
-  // if (frameCount % 80 == 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }
-  // }
-
-  // if (frameCount / 80 == 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
-  // }
-
-  // if (frameCount % 80 = 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
-  // }
+   
 
 
-
-}
-
+   if (frameCount % 80 == 0) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
+       createOrange();
+     }else{
+   createRed();
+     }
+    }
 function createApples() {
 apple = createSprite(random(50, 350),40, 10, 10);
 apple.addImage(appleImg);
@@ -105,10 +75,11 @@ orangeL.velocityY = 3;
 orangeL.lifetime = 150;
 }
 
-function createRed() {
-redL = createSprite(random(50, 350),40, 10, 10);
-redL.addImage(redImg);
-redL.scale=0.06;
-  redL.velocityY = 3;
-  redL.lifetime = 150;
+function createRed(){
+redL = createSprite(random(50,350),40,10,10);
+redL=addImage(redImg);
+redL.scale=0.08;
+redL.velocityY=3;
+redL.lifetime=150;
+}
 }
